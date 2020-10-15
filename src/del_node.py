40cedb2ef818f3118e850974cffc56d7a89ca3c9
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-15 17:58:10 -0600

    Renamed files

--- src/del_node.py
+++ src/del_node.py
@@ -3,7 +3,8 @@ Given only a reference to a specific node in a linked list, delete that node fro
 
 Example:
 
-The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`. It is your job to write the `delete_node` function.
+The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`. 
+It is your job to write the `delete_node` function.
 
 ```python
 class LinkedListNode():
@@ -21,7 +22,8 @@ y.next = z
 delete_node(y)
 ```
 
-*Note: We can do this in O(1) time and space! But be aware that our solution will have some side effects...*
+*Note: We can do this in O(1) time and space! 
+But be aware that our solution will have some side effects...*
 """
 class LinkedListNode():
     def __init__(self, value):
